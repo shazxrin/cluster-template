43d856240cc9c8e23b19ccf90600283da25a5dc6
shazrin <mshazrinbs@gmail.com>
2025-10-18 23:16:20 +0800

    fix: script should point to extra

--- install-applications.sh
+++ install-applications.sh
@@ -3,9 +3,9 @@
 echo "Adding repository for ArgoCD"
 kubectl apply -n argocd -f ./argocd/repository/cluster-repository-sealedsecret.yaml
 
-echo "Installing extras"
-kubectl apply -n argocd -f ./argocd/extras/reloader-application.yaml
-kubectl apply -n argocd -f ./argocd/extras/tailscale-application.yaml
+echo "Installing extra"
+kubectl apply -n argocd -f ./argocd/extra/reloader-application.yaml
+kubectl apply -n argocd -f ./argocd/extra/tailscale-application.yaml
 
 echo "Installing applications"
 for app_file in ./argocd/application/*-application.yaml; do
